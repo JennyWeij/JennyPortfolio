6a78d3696f7b4f40f4e4d9836342805f7681ba1e
JennyWeij <jennyweij@outlook.com>
2024-02-16 15:03:31 +0100

    added some light styling and installations

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box
+      style={{
+        margin: 0,
+        padding: 0,
+        height: "100vh",
+        background:
+          "linear-gradient(151deg, rgba(34,72,112,1) 43%, rgba(68,207,203,1) 100%)",
+      }}
+    ></Box>
+  );
 }
 
-export default App
+export default App;
