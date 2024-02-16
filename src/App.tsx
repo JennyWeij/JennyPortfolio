0cdb07b6463523f6c7d184fc382da4731f04f175
JennyWeij <jennyweij@outlook.com>
2024-02-16 15:22:46 +0100

    added image

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Image } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Flex
-      style={{
-        height: "100vh",
-        background:
-          "linear-gradient(151deg, rgba(34,72,112,1) 43%, rgba(68,207,203,1) 100%)",
-      }}
-      align="center"
-      justify="flex-end"
-    >
-      <Box
+    <Container>
+      <Flex
         style={{
-          height: "400px",
-          width: "400px",
-          marginRight: "150px",
-          backgroundColor: "#224870",
+          height: "100vh",
+          background:
+            "linear-gradient(151deg, rgba(34,72,112,1) 43%, rgba(68,207,203,1) 100%)",
         }}
-      ></Box>
-    </Flex>
+        align="center"
+        justify="flex-end"
+      >
+        <Box
+          style={{
+            height: "400px",
+            width: "400px",
+            marginRight: "10%",
+            backgroundColor: "#224870",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box boxSize="sm">
+            <Image
+              boxSize="280px"
+              objectFit="cover"
+              src="../public/cv.jpg"
+              alt="Picture of Jenny"
+            />
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
   );
 }
 
